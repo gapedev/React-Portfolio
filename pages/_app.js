c3ca26e1abe373b90c926ad741f82c45ebab6109
Gabi Pérez <maquetacion@clicktotravel.es>
2022-09-20 13:07:59 +0200

    prueba sass 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../scss/index.scss';
+import '../styles/styles.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-    <h1>Hola</h1>
+    <h1 className="titulo">Hola</h1>
     </>
   )
 }
