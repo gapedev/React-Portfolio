fc04413b3f7b8ed4e1fb99728393102bd3a05da7
Gabi Pérez <maquetacion@clicktotravel.es>
2022-11-20 11:24:07 +0100

    Iconos prueba componente

--- components/presentacion.jsx
+++ components/presentacion.jsx
@@ -1,6 +1,9 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import gabi from '../public/img/gabi.png';
+import Github from './icons/github';
 import Boton from '../components/boton'
+import Linkedin from './icons/linkedin';
 
 const Presentacion = () => {
     return (
@@ -12,12 +15,15 @@ const Presentacion = () => {
             <div className='presentacion_text'>
                 <h1>¡Hola!</h1>
                 <h2>Soy Gabi Pérez, Front-End Developer.</h2>
+                <div className='presentacion_about'>
                 <Boton 
                 text="Descargar CV"/>
-                <Boton
-                text="Prueba"/>
-                <div className='presentacion_about'>
-                <p>Conóceme</p>
+                <Link href="www.google.es">
+                    <Github width={40} height={40} fill={'black'}></Github>
+                </Link>
+                <Link href="www.google.es">
+                    <Linkedin width={40} height={40} fill="black"></Linkedin>
+                </Link>
                 </div>
             </div>
         </div>
